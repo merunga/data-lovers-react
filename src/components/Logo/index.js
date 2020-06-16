89d6c4dfd75a982b13f9b45f75a0868a45e5f2df
merunga <merunga@gmail.com>
2020-06-16 07:53:51 -0500

    Logo component

---
+++ src/components/Logo/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Logo = () => (
+  <img
+    src="https://upload.wikimedia.org/wikipedia/en/c/c8/Rick_and_Morty_logo.png"
+    className="App-logo"
+    alt="logo"
+  />
+);
+
+export default Logo;
