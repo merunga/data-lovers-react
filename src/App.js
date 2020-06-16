268e06d080dccc3e1e54f4af4fce3fc314f0b99b
merunga <merunga@gmail.com>
2020-06-16 14:49:18 -0500

    placeholders para componentes principales

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Header from './components/Header';
+import Filter from './components/Filter';
+import Content from './components/Content';
 
 function App() {
   return (
     <Container>
       <Header />
+      <Filter />
+      <Content />
     </Container>
   );
 }
---
+++ src/components/Content/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Content = () => (<div>Contenido</div>);
+
+export default Content;
