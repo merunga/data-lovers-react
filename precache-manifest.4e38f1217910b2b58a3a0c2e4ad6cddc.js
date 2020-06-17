6bfe5111cf80c9bcee2feefbc32db041583ec992
merunga <merunga@gmail.com>
2020-06-16 22:34:24 -0500

    Updates

---
+++ precache-manifest.4e38f1217910b2b58a3a0c2e4ad6cddc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da7af017792f5ea573b8c84111808b4",
+    "url": "/data-lovers-react/index.html"
+  },
+  {
+    "revision": "6456bcec6bc4676d72a8",
+    "url": "/data-lovers-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "69d121dccac74761b6af",
+    "url": "/data-lovers-react/static/css/main.6ff8d4ee.chunk.css"
+  },
+  {
+    "revision": "6456bcec6bc4676d72a8",
+    "url": "/data-lovers-react/static/js/2.8216fe0f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/data-lovers-react/static/js/2.8216fe0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d121dccac74761b6af",
+    "url": "/data-lovers-react/static/js/main.8eabee8a.chunk.js"
+  },
+  {
+    "revision": "462ac96adac607031928",
+    "url": "/data-lovers-react/static/js/runtime-main.a77fd57a.js"
+  }
+]);
